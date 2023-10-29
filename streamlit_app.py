2441995bed72d46d3e9625fa5ff819ea6e8c32ca
KunalDeb-Git <126451218+kunal-debnath-git@users.noreply.github.com>
2023-10-30 01:00:33 +0530

    with more def

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,21 +35,26 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+
+
+
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
 
-my_cur.execute("SELECT * from fruit_load_list")
-my_data_row = my_cur.fetchone()
-streamlit.header("the fruit load list containder")
-streamlit.dataframe(my_data_row)
+streamlit.header("The Fruit load list component")
+def get_fruit_load_list():
+  with my_cnx.cursor() as mycur:
+    my_cur.execute("SELECT * from fruit_load_list")
+    return my_data_row = my_cur.fetchall()
 
-my_data_row = my_cur.fetchall()
-streamlit.header("ALL the fruit load list containder")
-streamlit.dataframe(my_data_row)
+if streamlit.button('get fruit load list'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_row = get_fruit_load_list()
+  streamlit.dataframe(my_data_row)
+  
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
 streamlit.write('The user entered ', add_my_fruit)
@@ -92,6 +97,8 @@ try:
     streamlit.dataframe(back_from_function)
 except URLError as e:
   streamlit.error()
+
+
 
 
 streamlit.text('*End*')
