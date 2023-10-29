a8bddc14cec937edd2f35b5bc74ea868d93d3179
KunalDeb-Git <126451218+kunal-debnath-git@users.noreply.github.com>
2023-10-29 19:26:48 +0530

    added json response

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,6 +25,6 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"
 streamlit.text(fruityvice_response)
 
 streamlit.header("Fruityvice Fruit Advice!")
-
+streamlit.text(fruityvice_response.json())
 
 streamlit.text('*End*')
